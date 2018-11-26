023bd8ed7d4a024a808dc33835a50278f8a45cb4
G33tha <geetha.p@optit.co>
2018-11-26 11:59:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # Program to find the sum of all numbers stored in a list
 
 # List of numbers
-numbers = [8, 5, 3, 8, 4, 2, 5, 4, 11]
+numbers = [7, 5, 3, 8, 4, 2, 5, 4, 11]
 
 # variable to store the sum
 sum = 0
